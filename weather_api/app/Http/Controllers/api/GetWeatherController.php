fd98d331d889068eb9ecc7b9c00be7222cbf0007
DenisAlal <denisalalykin2001@gmail.com>
2023-08-10 18:41:27 +0700

    fixSSL

--- weather_api/app/Http/Controllers/api/GetWeatherController.php
+++ weather_api/app/Http/Controllers/api/GetWeatherController.php
@@ -14,16 +14,18 @@ class GetWeatherController extends Controller
         $lon = $request->input('lon');
         $lang = $request->input('lang');
         $units = $request->input('units');
-        $url = "https://api.openweathermap.org/data/2.5/forecast?lat=".$lat."&lon=".$lon."&units=".$units."&lang=".$lang."&appid=8c4d673ca47a9bd9ff436dcdee56e3bf";
+        $url = "http://api.openweathermap.org/data/2.5/forecast?lat=".$lat."&lon=".$lon."&units=".$units."&lang=".$lang."&appid=8c4d673ca47a9bd9ff436dcdee56e3bf";
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post($url, [
+            'verify' => false
+        ]);
 
-        $response = Http::post($url);
-
-        if ($response->successful()) {
-            $data = $response->json();
+        if ($response->getStatusCode() == 200) {
+            $data = json_decode($response->getBody()->getContents(), true);
             return response()->json([$data], 200);
         } else {
-            $statusCode = $response->status();
-            $errorMessage = $response->json();
+            $statusCode = $response->getStatusCode();
+            $errorMessage = $response->getBody()->getContents();
             return response()->json([
                 'message' => 'Ошибка запроса к API',
                 'status' => $statusCode,
@@ -37,14 +39,18 @@ class GetWeatherController extends Controller
         $q = $request->input('q');
         $lang = $request->input('lang');
         $units = $request->input('units');
-        $url = "https://api.openweathermap.org/data/2.5/forecast?q=".$q."&units=".$units."&lang=".$lang."&appid=8c4d673ca47a9bd9ff436dcdee56e3bf";
-        $response = Http::post($url);
-        if ($response->successful()) {
-            $data = $response->json();
+        $url = "http://api.openweathermap.org/data/2.5/forecast?q=".$q."&units=".$units."&lang=".$lang."&appid=8c4d673ca47a9bd9ff436dcdee56e3bf";
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post($url, [
+            'verify' => false
+        ]);
+
+        if ($response->getStatusCode() == 200) {
+            $data = json_decode($response->getBody()->getContents(), true);
             return response()->json([$data], 200);
         } else {
-            $statusCode = $response->status();
-            $errorMessage = $response->json();
+            $statusCode = $response->getStatusCode();
+            $errorMessage = $response->getBody()->getContents();
             return response()->json([
                 'message' => 'Ошибка запроса к API',
                 'status' => $statusCode,
@@ -52,4 +58,5 @@ class GetWeatherController extends Controller
             ], $statusCode);
         }
     }
+
 }
